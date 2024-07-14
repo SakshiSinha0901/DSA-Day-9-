2189e73449167457052f328f5a08624465e92aa9
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-07-14 22:44:50 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void print2DArray(int arr[][4], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int arr[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+    
+    cout << "2D Array Elements:" << endl;
+    print2DArray(arr, 3, 4);
+    
+    return 0;
+}
